6435e91ac881606ca45c92ba33dabd31cdc6e369
denpoya <dennpoya@gmail.com>
2013-04-18 15:54:10 +0900

    抽象クラスと継承クラス

--- MoodleQuizXMLMaker/Bean/AbstractBean.php
+++ MoodleQuizXMLMaker/Bean/AbstractBean.php
@@ -7,50 +7,51 @@
  * To change this template use File | Settings | File Templates.
  */
 
-namespace Bean;
+namespace Bean ;
 
+    /**
+     * Class AbstractBean
+     * @package beans
+     * 問題1つに対して、1つのこれをextendsしたクラスが担当する。
+     *
+     */
+    /*
+    <question type="truefalse">
+        <name>
+          <text>問1</text>
+        </name>
+        <questiontext format="html">
+          <text><![CDATA[<p>CoffeeScriptは美味しい。</p>]]></text>
+        </questiontext>
+        <generalfeedback format="html">
+          <text><![CDATA[<p>CoffeeScriptは飲めませんよねｗ</p>]]></text>
+        </generalfeedback>
+        <defaultgrade>1.0000000</defaultgrade>
+        <penalty>1.0000000</penalty>
+        <hidden>0</hidden>
+        <answer fraction="0" format="moodle_auto_format">
+          <text>true</text>
+          <feedback format="html">
+            <text><![CDATA[<p>正解！</p>]]></text>
+          </feedback>
+        </answer>
+        <answer fraction="100" format="moodle_auto_format">
+          <text>false</text>
+          <feedback format="html">
+            <text><![CDATA[<p>はずれ！</p>]]></text>
+          </feedback>
+        </answer>
+        <tags>
+          <tag><text>JacaScript</text></tag></tags>
+      </question>
+     *
+     */
+    abstract class AbstractBean {
 
-/**
- * Class AbstractBean
- * @package beans
- * 問題1つに対して、1つのこれをextendsしたクラスが担当する。
- *
- */
-/*
-<question type="truefalse">
-    <name>
-      <text>問1</text>
-    </name>
-    <questiontext format="html">
-      <text><![CDATA[<p>CoffeeScriptは美味しい。</p>]]></text>
-    </questiontext>
-    <generalfeedback format="html">
-      <text><![CDATA[<p>CoffeeScriptは飲めませんよねｗ</p>]]></text>
-    </generalfeedback>
-    <defaultgrade>1.0000000</defaultgrade>
-    <penalty>1.0000000</penalty>
-    <hidden>0</hidden>
-    <answer fraction="0" format="moodle_auto_format">
-      <text>true</text>
-      <feedback format="html">
-        <text><![CDATA[<p>正解！</p>]]></text>
-      </feedback>
-    </answer>
-    <answer fraction="100" format="moodle_auto_format">
-      <text>false</text>
-      <feedback format="html">
-        <text><![CDATA[<p>はずれ！</p>]]></text>
-      </feedback>
-    </answer>
-    <tags>
-      <tag><text>JacaScript</text></tag></tags>
-  </question>
- *
- */
-abstract class AbstractBean {
+        private $config ;  //stdClass
+        private $question ;//string;
+        private $answer ;  //string
 
-    public function getXMLData(){
 
     }
-}
 
--- MoodleQuizXMLMaker/Bean/TorFBean.php
+++ MoodleQuizXMLMaker/Bean/TorFBean.php
@@ -7,10 +7,41 @@
  * To change this template use File | Settings | File Templates.
  */
 
-namespace Bean;
+namespace Bean ;
 
-class TorFBean extends AbstractBean {
-    function getXMLData(){
+    class TorFBean extends AbstractBean {
+
+        private $config ;  //stdClass
+        private $question ;//string;
+        private $answer ;  //string
+
+        public function setConfig($config)
+        {
+            $this->config = $config;
+        }
+
+        public function getConfig()
+        {
+            return $this->config;
+        }
+
+        public function setQuestion($question)
+        {
+            $this->question = $question;
+        }
+
+        public function getQuestion()
+        {
+            return $this->question;
+        }
+        public function setAnswer($answer)
+        {
+            $this->answer = $answer;
+        }
+
+        public function getAnswer()
+        {
+            return $this->answer;
+        }
 
     }
-}
