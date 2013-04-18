1f3426c6110ad625f23569912ff40a19ec1e7254
denpoya <dennpoya@gmail.com>
2013-04-18 15:00:53 +0900

    抽象クラスと継承クラス

---
+++ MoodleQuizXMLMaker/Bean/TorFBeans.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: denn
+ * Date: 13/04/18
+ * Time: 13:27
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Bean;
+
+/**
+ * Class TorFBeans
+ * @package Bean
+ *
+ */
+class TorFBeans extends AbstractBeans {
+
+    protected $beans = array();
+    /**
+     *@param TorFBean $bean
+     */
+    function addBean($bean) {
+       $this->beans[] = $bean ;
+    }
+
+    /**
+     * @return TorFBean
+     *         空の場合NULLを返す。
+     */
+    function getBean() {
+            return array_shift($this->beans) ;
+    }
+}
